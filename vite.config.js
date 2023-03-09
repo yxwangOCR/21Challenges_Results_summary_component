8c6987ad9c7c2ad441085908a5bf91cfdc2e8839
Yixuan Wang <wang.yixuan@live.fr>
2023-03-09 18:27:49 -0500

    Deployed website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/yxwangOCR/',
+  base: '/21Challenges_Results_summary_component/',
   plugins: [react()],
 })
