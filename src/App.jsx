474489c3440eeb925d6d47904567c552f14def0f
Yixuan Wang <wang.yixuan@live.fr>
2023-03-09 16:10:16 -0500

    Add: header style

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,29 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! Hi
-    </h1>
+    <main className='min-w-[350px] min-h-screen'>
+      <header className='flex flex-col justify-center items-center text-center bg-gradient-to-b from-slateBlue to-royalBlue rounded-b-3xl gap-4 py-8 px-9'>
+        <h1 className='text-lightLavender'>Your Result</h1>
+        <div className='flex flex-col justify-center items-center rounded-full bg-gradient-to-b from-violetBlue to-slateBlue w-40 h-40 gap-2'>
+          <span className='text-white text-6xl'>76</span>
+          <span className='text-lightLavender'>of 100</span>
+        </div>
+        <h2 className='text-white text-3xl'>Great</h2>
+        <p className='text-lightLavender'>You scored higher than 65% of the people who have taken these tests.</p>
+      </header>
+
+      <section className='bg-red'>
+        <h3>Summary</h3>
+        <ul>
+          <li className='flex justify-between'><img src="" alt="" /> <h4>Reaction</h4> <div><strong>80<span> / 100</span></strong></div></li>
+          <li className='flex justify-between'><img src="" alt="" /> <h4>Reaction</h4> <div><strong>80<span> / 100</span></strong></div></li>
+          <li className='flex justify-between'><img src="" alt="" /> <h4>Reaction</h4> <div><strong>80<span> / 100</span></strong></div></li>
+          <li className='flex justify-between'><img src="" alt="" /> <h4>Reaction</h4> <div><strong>80<span> / 100</span></strong></div></li>
+        </ul>
+        <button>Continue</button>
+      </section>
+    </main>
+
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,42 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    colors: {
+      'red': 'hsl(0, 100%, 67%)',
+      'yellow': 'hsl(39, 100%, 56%)',
+      'green': 'hsl(166, 100%, 37%)',
+      'blue': 'hsl(234, 85%, 45%)',
+      'slateBlue': 'hsl(252, 100%, 67%)',
+      'royalBlue': 'hsl(241, 81%, 54%)',
+      'violetBlue': 'hsla(256, 72%, 46%, 1)',
+      'persianBlue': 'hsla(241, 72%, 46%, 0)',
+      'white': 'hsl(0, 0%, 100%)',
+      'paleBlue': 'hsl(221, 100%, 96%)',
+      'lightLavender': 'hsl(241, 100%, 89%)',
+      'grayBlue': 'hsl(224, 30%, 27%)'
+    },
+    fontFamily: {
+      text: ['Hanken Grotesk', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
